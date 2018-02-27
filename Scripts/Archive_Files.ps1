01e051a3625684519a0e09a3fea41420915475a5
Andrey Korneyev <32826269+XsuntSQL@users.noreply.github.com>
2018-02-26 22:22:35 -0500

    Reduced the compression ratio to speed up the process

--- Scripts/Archive_Files.ps1
+++ Scripts/Archive_Files.ps1
@@ -5,5 +5,5 @@ param
     [string]$Passw = $(Throw "Missing parameter: -Passw Passw")
 )
 $pathTo64Bit7Zip = "C:\Program Files\7-Zip\7z.exe";
-$Params = "a -tzip ""$ZipOutputFilePath"" ""$FilesToZip"" -mx9 -p""$Passw"""
+$Params = "a -tzip ""$ZipOutputFilePath"" ""$FilesToZip"" -mx5 -p""$Passw"""
 Start-Process $pathTo64Bit7Zip -ArgumentList $Params -Wait -PassThru -NoNewWindow
